27400e27055fbaf442932f2cea885d77acb907d8
Rcstuker <114772242+Rcstuker@users.noreply.github.com>
2023-06-14 17:48:48 -0300

    Add files via upload

---
+++ PRO-C24-ArqueiroEpico-Modelo-main/i.js
@@ -0,0 +1,48 @@
+// for (var i = 0, i++) {
+   //   if (playerArrows[i] !== undefined) {
+   //     playerArrows[i].display();
+   //   }
+   // }
+    // for (var i) {
+   //   if (playerArrows[i] !== undefined) {
+   //     playerArrows[i].display();
+   //   }
+   // }
+
+
+
+
+
+   // function keyPressed() {
+//   if (keyCode === 23) {
+//     var posX = playerArcher.body.position.x;
+//     var posY = playerArcher.body.position.y;
+//     var angle = playerArcher.body.angle;
+//     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+
+//     Matter.Body.setAngle(arrow.body, angle);
+//     playerArrows.push(arrow);
+//   }
+// }
+// function keyPressed() {
+//   if (keyCode === 32) {
+//     var posX = playerArcher.body.position.x;
+//     var posY = playerArcher.body.position.y;
+//     var angle = playerArcher.body.angle;
+//     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+
+//     Matter.Body.setAngle(arrow.body, angle);
+//     playerArrows(arrow);
+//   }
+// }
+// function keyPressed() {
+//   if (keyCode === 32) {
+//     var posX = playerArcher.body.position.x;
+//     var posY = playerArcher.body.position.y;
+//     var angle = playerArcher.body.angle;
+//     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+
+//     Matter.Body.setAngle(arrow.body, angle);
+//     playerArrows.push(arrow);
+//   }
+// }
